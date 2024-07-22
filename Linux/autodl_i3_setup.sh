aa36ba6e0273a7f9b41e7f8d9c49733e6885bdbe
Livakivi <130640138+Livakivi@users.noreply.github.com>
2024-07-22 09:01:09 +0900

    Create autodl_i3_setup.sh

---
+++ Linux/autodl_i3_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Temporary directory to clone the repository
+temp_dir=$(mktemp -d)
+repo_url="https://github.com/Livakivi/config-files.git"
+
+# Clone the repository
+git clone $repo_url $temp_dir
+
+# Move folders from the cloned repository to .config directory
+mv $temp_dir/Linux/.config/* ~/.config/
+mkdir -p ~/Documents/wallpapers
+mv $temp_dir/wallpapers/* ~/Documents/wallpapers/
+mkdir -p ~/scripts
+mv $temp_dir/Linux/scripts/* ~/scripts/
+chmod +x ~/scripts/*
+
+# Clean up - remove the temporary directory
+rm -rf $temp_dir
